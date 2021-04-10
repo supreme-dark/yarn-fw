6add9f2106c096fb929e917e2af132054740ffe3
supreme-dark <44330675+supreme-dark@users.noreply.github.com>
2021-04-10 15:02:52 +0800

    test

---
+++ src/Guidances/Crecognizer .cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <vector>
+
+#include "Crecognizer .h"
---
+++ src/Guidances/Crecognizer .h
@@ -0,0 +1,40 @@
+/*
+* tracer: class tracer extends guidance
+* SdtElectronics 2021.3
+*/
+
+#pragma once
+#include "../drivers/lStream.h"
+#include "../lib/slideAvg.h"
+#include "guidance.h"
+
+class Color_Recognizer: public guidance{
+public:
+    tracer(bool rhs, lightSS& sensor, size_t filterLen);
+
+private:
+    //Calibrate SQUAL ripple range
+    //Call before starting guidance
+    void sCalibrate(quiver& subject);
+    void tracerTask(quiver& subject);
+
+    lightSS& _sensor;
+    slideAvg<double> filter;
+
+    //Upper and lower limit of SQUAL
+    //Set by sCalibrate()
+    unsigned int sRippleUL;
+    unsigned int sRippleLL;
+
+    //Flag indicating whether rover is deviating
+    bool deviating;
+
+    //Speed deviation with direction
+    const int deviation;
+
+    //Absolute value of speed deviation
+    static constexpr int devBase = 10;
+
+    //Margin value for SQUAL ripple
+    static constexpr unsigned int sRippleMargin = 10;
+};
